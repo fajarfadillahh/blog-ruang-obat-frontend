9929987c51c250717290ef9a2d425da56b621382
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-11 20:01:53 +0700

    chore(components): remove unnesessary code on the navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,6 @@
 import { LogoRuangobat } from "@/assets/img/LogoRuangobat";
-import { useEffect, useState } from "react";
 
 export default function Navbar() {
-  const [city, setCity] = useState<string>("");
-  const [time, setTime] = useState<string>("");
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      const now = new Date();
-      setTime(
-        now.toLocaleTimeString("id-ID", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        }),
-      );
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-  useEffect(() => {
-    fetch("https://ipapi.co/json/")
-      .then((res) => res.json())
-      .then((data) => setCity(`${data.city}, ${data.country_name}`))
-      .catch(() => setCity("Lokasi tidak diketemu!"));
-  }, []);
-
-  console.log(city);
-  console.log(time);
-
   return (
     <nav className="mx-auto flex h-full w-full max-w-[1440px] items-center justify-center border-b-2 border-gray-900/5 px-4 lg:px-0">
       <a href="/" className="inline-flex items-center gap-2">
