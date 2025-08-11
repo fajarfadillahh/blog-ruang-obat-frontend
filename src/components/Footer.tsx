95dddff66d5b8ac7d5233f8b123cf66443c8b7f7
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-11 20:03:19 +0700

    style(components): responsive text size on footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="mx-auto mt-16 flex h-32 w-full max-w-[1440px] items-center justify-center border-t-2 border-gray-900/5 px-4 lg:px-0">
-      <p className="leading-[170%] font-medium text-gray-500">
+      <p className="text-center text-sm leading-[170%] font-medium text-gray-500 sm:text-base">
         &copy; RuangObat. All rights reserved -{" "}
         <a
           href="https://ruangobat.id"
