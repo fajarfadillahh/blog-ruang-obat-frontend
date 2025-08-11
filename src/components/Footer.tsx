21872ed46e34a7d3ef9bc48880a849bf52f95d34
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2025-08-11 20:01:19 +0700

    style(components): modified footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,16 @@
 export default function Footer() {
   return (
-    <div className="mx-auto flex h-full w-full max-w-[1440px] items-center px-4 lg:px-0">
-      Footer
+    <div className="mx-auto mt-16 flex h-32 w-full max-w-[1440px] items-center justify-center border-t-2 border-gray-900/5 px-4 lg:px-0">
+      <p className="leading-[170%] font-medium text-gray-500">
+        &copy; RuangObat. All rights reserved -{" "}
+        <a
+          href="https://ruangobat.id"
+          target="_blank"
+          className="text-ruangobat font-semibold underline"
+        >
+          RuangObat.
+        </a>
+      </p>
     </div>
   );
 }
